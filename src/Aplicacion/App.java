7274d73660054d6cfc215b0e0470001a97296a04
Alexander Olmedo <olmedo2408@gmail.com>
2017-05-29 13:33:56 -0500

    Añadido SplashScreen al inicio

--- src/Aplicacion/App.java
+++ src/Aplicacion/App.java
@@ -1,20 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Aplicacion;
 
 import Interfaces.*;
+import javax.swing.JFrame;
 
-/**
- *
- * @author vengatus
- */
 public class App {
     public static void main(String args[]) {
 
+        SplashScreen inicio = new SplashScreen();
+        inicio.setLocationRelativeTo(null);
+        inicio.setVisible(true);
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+        inicio.setVisible(false);
+        inicio.dispose();
+        
         new Login().setVisible(true);
+        
         //System.setProperty("user.dir", "/home/andreu/NetBeansProjects/AplicacionesLibres/src");
         //new VentanaPrincipal("1723123459",2016).setVisible(true);
         //new VentanaPrincipal("1718269671",2016).setVisible(true);
