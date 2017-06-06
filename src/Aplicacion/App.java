e132406f6acb2923e3d5801dc12faf8bd0114bd0
mayliz95 <mayliz.95@hotmail.com>
2017-06-06 09:21:07 -0500

    Splash y formato!!!

--- src/Aplicacion/App.java
+++ src/Aplicacion/App.java
@@ -3,7 +3,9 @@ package Aplicacion;
 import Interfaces.*;
 import conexionBDD.InitDatabase;
 import java.io.File;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.UIManager;
 
 public class App {
     public static void main(String args[]) {
@@ -11,7 +13,24 @@ public class App {
         if(!base.exists()){
             InitDatabase crearBase = new InitDatabase();
         }
-        new Login().setVisible(true);
+        try{
+  
+            JFrame.setDefaultLookAndFeelDecorated(true);
+            JDialog.setDefaultLookAndFeelDecorated(true);
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+            //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+      }
+      catch (Exception e)
+       {
+        e.printStackTrace();
+       }
+        
+        splash s = new splash();
+        s.setVisible(true);
+        s.iniciar();
+        //new Login().setVisible(true);
         
     }    
 }
--- src/Aplicacion/splash.java
+++ src/Aplicacion/splash.java
@@ -8,6 +8,7 @@ import javax.swing.JPanel;
 import Interfaces.*;
 import conexionBDD.InitDatabase;
 import java.io.File;
+import javax.swing.UIManager;
 
 /**
  *
@@ -96,7 +97,7 @@ public class splash extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
+        /*Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
@@ -108,6 +109,8 @@ public class splash extends javax.swing.JFrame {
                     break;
                 }
             }
+            //javax.swing.UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
         } catch (ClassNotFoundException ex) {
             java.util.logging.Logger.getLogger(splash.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
